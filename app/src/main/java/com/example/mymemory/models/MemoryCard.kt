93f8a127b8dacf2963663eb3190d94a62ae4a3be
MukumovMuhammad <muhammad.bw.03@gmail.com>
2024-11-09 15:17:46 +0500

    First commit

---
+++ app/src/main/java/com/example/mymemory/models/MemoryCard.kt
@@ -0,0 +1,9 @@
+package com.example.mymemory.models
+
+import org.intellij.lang.annotations.Identifier
+
+data class MemoryCard(
+    val identifier: Int,
+    var isFaceUp: Boolean = false,
+    var isMatched: Boolean = false
+)
---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_alarm,
+    R.drawable.ic_home,
+    R.drawable.ic_car,
+    R.drawable.ic_moon,
+    R.drawable.ic_work,
+    R.drawable.ic_flash,
+    R.drawable.ic_school,
+    R.drawable.ic_science,
+    R.drawable.ic_heart,
+    R.drawable.ic_phone,
+    R.drawable.ic_plane,
+    R.drawable.ic_smile,
+
+)
