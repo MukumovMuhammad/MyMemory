2b86e9030e49d0a479778ada3e7e2749b6a5914d
MukumovMuhammad <muhammad.bw.03@gmail.com>
2024-11-23 12:42:57 +0500

    Enabling save button

--- app/src/main/java/com/example/mymemory/utils/CreateActivity.kt
+++ app/src/main/java/com/example/mymemory/utils/CreateActivity.kt
@@ -1,7 +1,18 @@
 package com.example.mymemory.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,35 +21,153 @@ import com.example.mymemory.R
 import com.example.mymemory.models.BoardSize
 
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CreateActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "CreateActivity"
+        private const val PICK_PHOTO_CODE = 655 // Not necessary 655 can be any number
+        private const val READ_EXTERNAL_PHOTOS_CODE = 248 // same as 655
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    private lateinit var rvImagePicker: RecyclerView
+    private lateinit var btnSave: Button
+    private lateinit var etGameName: EditText
+
+
+    private lateinit var adapter: ImagePickerAdapter
     private lateinit var boardSize : BoardSize
-    private var numImagesREquired = -1
+    private var numImagesRequired = -1
+    private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        rvImagePicker = findViewById(R.id.rvImagePicker)
+        etGameName = findViewById(R.id.etGameName)
+        btnSave = findViewById(R.id.btnSave)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
-        numImagesREquired = boardSize.getNumPairs()
-        supportActionBar?.title = "Choose pics (0/ $numImagesREquired)"
+        numImagesRequired = boardSize.getNumPairs()
+        supportActionBar?.title = "Choose pics (0/ $numImagesRequired)"
+
+        etGameName.filters = arrayOf(InputFilter.LengthFilter(14))
+        etGameName.addTextChangedListener(object : TextWatcher{
+
+            override fun afterTextChanged(s: Editable?) {
+                btnSave.isEnabled = shouldEnableSaveButton()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+
+
+        })
+        adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener{
+            override fun onPlaceholderClicked() {
+                if (isPermissionGranted(this@CreateActivity,READ_PHOTOS_PERMISSION,  )){
+                    launchIntentForPhotos()
+                }else{
+                    requestPermission(this@CreateActivity,READ_PHOTOS_PERMISSION ,READ_EXTERNAL_PHOTOS_CODE)
+                }
+
+            }
+
+
+        })
+
+        rvImagePicker.adapter = adapter
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
 
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_EXTERNAL_PHOTOS_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                launchIntentForPhotos()
+            }else{
+                Toast.makeText(this, "In order to create a custom game you need to provide access to yout photos", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home){
             finish()
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode != PICK_PHOTO_CODE || resultCode != Activity.RESULT_OK || data == null){
+            Log.w(TAG, "Did not get data back. user likely canceled flow")
+            Log.w(TAG, "Result ${resultCode} photo code ${PICK_PHOTO_CODE} Result ok ${Activity.RESULT_OK}")
+            Log.w(TAG, "data ${data}")
+            return
+        }
+        val selectedUri = data.data
+        val clipData = data.clipData
+
+        if (clipData != null){
+            Log.i(TAG, "clipData numImages ${clipData.itemCount}: ${clipData}")
+            for (i in 0 until clipData.itemCount){
+                val clipItem = clipData.getItemAt(i)
+                if (chosenImageUris.size < numImagesRequired){
+                    chosenImageUris.add(clipItem.uri)
+                }
+            }
+        }else if (selectedUri != null){
+            Log.i(TAG, "data ${selectedUri}")
+            chosenImageUris.add(selectedUri)
+        }
+
+        adapter.notifyDataSetChanged()
+        supportActionBar?.title = "Choose pics (${chosenImageUris.size} / ${numImagesRequired})"
+        btnSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    private fun shouldEnableSaveButton(): Boolean {
+
+        if (chosenImageUris.size != numImagesRequired){
+            return false;
+        }
+
+        if (etGameName.text.isBlank() || etGameName.text.length < 3){
+            return false;
+        }
+
+
+        return true
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Choose pics"), PICK_PHOTO_CODE)
     }
 }
